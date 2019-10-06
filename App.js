38bd6b0c794cdc00cf8b53b5032ed1ec8752695d
thisfikri <32200380+thisfikri@users.noreply.github.com>
2019-10-06 22:05:15 +0700

    update_todo_repo_update1

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ class App extends Component {
     super(props);
     this.state = {
       todoListItems: [
-        {id:1, name:'Work', done: false},
-        {id:2, name:'run', done: false},
-        {id:3, name:'play', done: false},
-        {id: 4, name:'swim', done: false}
+        {id: 1, name: 'Work', done: false},
+        {id: 2, name: 'run', done: false},
+        {id: 3, name: 'play', done: false},
+        {id: 4, name: 'swim', done: false}
       ],
       value: '',
       btnTitle: 'Add',
@@ -23,7 +23,7 @@ class App extends Component {
     if (action == 'Add') {
       let currId = this.state.todoListItems.length
       this.setState((state) => {
-      const list = state.todoListItems.push({
+      let list = state.todoListItems.push({
         id: currId + 1,
         name: this.state.value
       })
@@ -44,9 +44,7 @@ class App extends Component {
     for (let i = 0; i < this.state.todoListItems.length; i++) {
       if (this.state.todoListItems[i].id == (id)) {
         this.setState((state) => {
-          const list = state.todoListItems.splice(i, 1)
-
-          return list
+          return state.todoListItems.splice(i, 1)
         })
       }
     }
@@ -74,13 +72,15 @@ class App extends Component {
       <SafeAreaView>
         <View style={{flexDirection: 'row', margin: 5}}>
           <TextInput
-          style={{flex: 1, borderWidth: 1, marginRight: 5, borderColor: 'gray', borderRadius: 3}}
-          onChangeText = {(text) => this.setState({value: text})}
-          value = {this.state.value}
+            style={{flex: 1, borderWidth: 1, marginRight: 5, borderColor: 'gray', borderRadius: 3}}
+            onChangeText = {(text) => this.setState({value: text})}
+            onPress={() => this.deleteTodoList(data.id)}  value = {this.state.value}
           />
           <Button light
-          style={{borderRadius: 5, fontWeight: 'bold', padding: 10, height: 50, borderWidth: 1, borderColor: 'gray'}}
-          onPress ={() => this.addTodoList(this.state.btnTitle)}><Text>{this.state.btnTitle}</Text></Button>
+            style={{borderRadius: 5, fontWeight: 'bold', padding: 10, height: 50, borderWidth: 1, borderColor: 'gray'}}
+            onPress ={() => this.addTodoList(this.state.btnTitle)}>
+            <Text>{this.state.btnTitle}</Text>
+          </Button>
         </View>
         <View>
           {this.state.todoListItems.map((data) =>
@@ -89,10 +89,17 @@ class App extends Component {
                 value={data.done}
                 onValueChange={() => this.doneTodoList(data.id)}
               />
-
               <Text style={{flex: 8, paddingVertical: 10}}>{data.name}</Text>
-              <Button onPress={() => this.editTodo(data.id, data.name)} style={{paddingHorizontal: 10, marginRight: 5, backgroundColor: "none", borderWidth: 1, borderColor: 'grey'}}><Icon name="pencil-alt" size={20} color="green" /></Button>
-              <Button onPress={() => this.deleteTodoList(data.id)} style={{paddingHorizontal: 10, backgroundColor: "none", borderWidth: 1, borderColor: 'grey'}}><Icon name="trash-alt" size={20} color="red" /></Button>
+              <Button
+                style={{paddingHorizontal: 10, marginRight: 5, backgroundColor: "none", borderWidth: 1, borderColor: 'grey'}}
+                onPress={() => this.editTodo(data.id, data.name)}>
+                <Icon name="pencil-alt" size={20} color="green" />
+              </Button>
+              <Button
+                style={{paddingHorizontal: 10, backgroundColor: "none", borderWidth: 1, borderColor: 'grey'}}
+                onPress={() => this.deleteTodoList(data.id)} >
+                <Icon name="trash-alt" size={20} color="red" />
+              </Button>
             </View>
           )}
         </View>
